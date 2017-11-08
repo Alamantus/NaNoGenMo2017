7d32d06b238cffd1c0cc209c9b338d94d67b1bd8
Robbie Antenesse <dev@alamantus.com>
2017-11-07 22:39:51 -0700

    Make Describer.language() generate something

--- src/Describer.js
+++ src/Describer.js
@@ -11,48 +11,69 @@ import {
 import sentenceBuilder from './sentenceBuilder';
 
 class Describer {
-  constructor (random) {
+  constructor ({random, toneManager, language = undefined}) {
     this.random = random;
     this.randomArrayValue = (array) => randomArrayValue(array, this.random);
+    this.toneManager = toneManager;
+    this.language = language || require('./LanguageGenerator')(this.random);
   }
 
   location ({
-    language = null,
     placeName = 'it',
-    feeling = null,
     numSentences = undefined,
   } = {}) {
-    if (!language) language = require('./LanguageGenerator')(this.random);
-    if (!feeling) {
-      switch (Math.ceil(this.random() * 5)) {
+    const sentences = [];
+    const numberOfSentences = numSentences || Math.ceil(this.random() * MAX_SENTENCES);
+    for (let i = 0; i < numberOfSentences; i++) {
+      const subject = {
+        noun: (i === 0) ? placeName : 'it',
+      };
+      const action = {
+        verb: 'is',
+      };
+      const object = {};
+
+      switch (i) {
+        case 0: {
+          if (this.random() > 0.5) {
+            action.verb = 'was';
+            object.adjective = 'once';
+          }
+          object.noun = 'home';
+          object.prepositional = 'to the ' + this.language.name + ' people';
+          break;
+        }
         case 1: {
-          feeling = POSITIVE;
+          action.verb = 'used to be';
+          object.adjective = this.toneManager.chooseOppositeToneWord('adjectives');
+          object.prepositional = 'but is now ' + this.toneManager.chooseTonallyAppropriateWord('adjectives');
           break;
         }
         case 2: {
-          feeling = NEGATIVE;
-          break;
-        }
-        case 3: {
-          feeling = FEARFUL;
-          break;
-        }
-        case 4: {
-          feeling = EXCITED;
+          subject.noun = 'anyone';
+          subject.prepositional = 'who has been there';
+          action.verb = 'would tell you that ' + placeName + ' is';
+          object.adjective = this.toneManager.getRandomWord('adjectives');
+          object.prepositional = 'yet ' + this.toneManager.chooseTonallyAppropriateWord('adjectives');
           break;
         }
         default: {
-          feeling = NEUTRAL;
+          object.adjective = this.toneManager.chooseTonallyAppropriateWord('adjectives');
           break;
         }
       }
+
+      const sentence = sentenceBuilder({
+        subject,
+        action,
+        object,
+      });
+
+      sentences.push(sentence);
     }
 
-    const numberOfSentences = numSentences || Math.ceil(this.random() * MAX_SENTENCES);
-    for (let i = 0; i < numberOfSentences; i++) {
-      
-    }
+    return sentences.join(' ');
   }
 }
 
-module.exports = (random) => new Describer(random);
+module.exports = (settings) => new Describer(settings);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import seedrandom from 'seedrandom';
-const random = seedrandom('testseed');
+const random = seedrandom();
 
 console.log('Generating!');
 
 const language = require('./LanguageGenerator')(random);
-console.log(language.test());
+// console.log(language.test());
 
 const toneManager = require('./ToneManager')(random);
-console.log(toneManager.currentTone);
-let output = '';
-for (let i = 0; i < 50; i++) {
-  output += toneManager.chooseTonallyAppropriateWord('adjectives');
-  output += '(' + toneManager.toneLevel.toString() + ': ' + toneManager.currentTone + ')';
-  output += ' ';
-}
-console.log(output);
+// console.log(toneManager.currentTone);
+// let output = '';
+// for (let i = 0; i < 50; i++) {
+//   output += toneManager.chooseTonallyAppropriateWord('adjectives');
+//   output += '(' + toneManager.toneLevel.toString() + ': ' + toneManager.currentTone + ')';
+//   output += ' ';
+// }
+
+const describe = require('./Describer')({
+  random,
+  toneManager,
+  language
+});
+
+console.log(describe.location({placeName: 'the quarry'}));
